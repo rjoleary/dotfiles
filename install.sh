236e67fbe226dd41bc46bd0527753cc60a652c88
Ryan O'Leary <ryan@ryanoleary.ca>
2015-02-03 01:09:46 -0500

    Avoid submodules for installations

--- install.sh
+++ install.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
 
-# Initialize and update git submodules.
-git submodule update --init
-
 # Add symbolic links.
 ln -s `pwd`/vimrc ~/.vimrc
 ln -s `pwd`/vim ~/.vim
@@ -13,4 +10,5 @@ ln -s `pwd`/zsh ~/.zsh
 chsh -s /usr/bin/zsh
 
 # Install bundles into vim.
+git clone https://github.com/gmarik/Vundle.vim.git vim/bundle/vundle
 vim +BundleInstall +qall
