cfe4f51ae12ce222fd1cc4d4ea88feb3e13da3b1
Ryan O'Leary <ryan@ryanoleary.ca>
2016-03-19 01:49:11 -0400

    Fix some issues found when testing the installation script

--- install.sh
+++ install.sh
@@ -1,11 +1,15 @@
 #!/bin/sh
 
+# Install git and zsh if they are not yet installed.
+sudo apt-get install git zsh
+
 # Install git prompt for zsh.
-git clone https://github.com/olivierverdier/zsh-git-prompt.git zsh/git-prompt
+git clone https://github.com/olivierverdier/zsh-git-prompt.git .zsh/git-prompt
 
 # Change the default shell to zsh.
+echo 'Your password might be needed to change the default shell.'
 chsh -s /usr/bin/zsh
 
 # Install bundles into vim.
-git clone https://github.com/gmarik/Vundle.vim.git vim/bundle/vundle
+git clone https://github.com/gmarik/Vundle.vim.git .vim/bundle/vundle
 vim +BundleInstall +qall
